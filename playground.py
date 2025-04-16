66b28cdd8ee95d94944c66b01731f80d70a94d10
Lena Amrein <lena_amrein@sluz.ch>
2025-04-16 10:53:53 +0200

    def.

--- playground.py
+++ playground.py
@@ -76,51 +76,107 @@ def binary_search(arr, target):
 
 
 if __name__ == "__main__":
-    if add(1,2) == 3:
-        print("Add Test bestanden")
-
-    if subtract(5,10) == (-5):
-        print("Subtract Test bestanden")
     
-    if multiply(0,5) == 0:
-        print("Multiply Test bestanden")
+    def test_add():
+        assert add(1,3) == 4
+        assert add(-2,-3) == -5
+        assert add(5,-5) == 0
+        assert add(1000000, 1) == 1000001
+        print("add Test bestanden")
 
-    if divide(0,0) == None:
-        print("Divide Test bestanden")
-    else: print("Divide Test nicht bestanden")
+    def test_subtract():
+        assert subtract(9,7) == 2
+        assert subtract(5, 7) == -2
+        assert subtract(5,5) == 0
+        assert subtract(-5, -100) == 95
+        print("substract Test bestanden")
 
-    if is_even(6)== True:
-        print("is even Test bestanden")
+    def test_multiply():
+        assert multiply(5,5) == 25
+        assert multiply(-5,5) == -25
+        assert multiply(0,5) == 0
+        assert multiply(-5,-5) == 25
+        print("multiply Test bestanden")
 
-    if max_of_two(-5,0) == 0:
-        print("max of two Test bestanden")
-    
-    if min_of_two(-1,-2) == -2:
-        print("min of two Test bestanden")
+    def test_divide():
+        assert divide(5,5) == 1
+        assert divide(5,-5) == -1
+        assert divide(-5,-5) == 1
+        assert divide(0,5) == 0
+        assert divide(0,0) == None
+        print("divide Test bestanden")
 
-    if factorial(4) == 24:
+    def test_is_even():
+        assert is_even(2) == True
+        assert is_even(-2) == True
+        assert is_even(0) == True
+        assert is_even(13) == False
+        print("is_even Test bestanden")
+
+    def test_max_of_two():
+        assert max_of_two(2,3) == 3
+        assert max_of_two(-10,-5) == -5
+        assert max_of_two(-3,0) == 0
+        assert max_of_two(-100,1) == 1
+        print("max_of_two Test bestanden")
+
+    def test_min_of_two():
+        assert min_of_two(2,3) == 2
+        assert min_of_two(-10,-5) == -10
+        assert min_of_two(3,0) == 0
+        assert min_of_two(-100,1) == -100
+        print("min_of_two Test bestanden")
+
+    def test_factorial():
+        assert factorial(5) == 120
+        assert factorial(-5) == None
+        assert factorial(0) == 1
+        assert factorial(2) == 2
         print("factorial Test bestanden")
-    
-    if is_palindrome(1221) == 1:
+    #Es kann Dezimalzahlen nicht berechnen
+
+    def test_is_palindrome():
+        assert is_palindrome(1221) == True
+        assert is_palindrome(23234) == False
         print("is_palindrome Test bestanden")
 
-    if count_vowels("Ich bin am töstön") == 5:
-        print("Vokal Test bestanden")
-    # Die Vokale ÄÖÜ sind nicht vorhanden
+    def test_count_vowels():
+        assert count_vowels("Hallo, mir geht es gut") == 6
+        assert count_vowels("jkdlfk") == 0
+        assert count_vowels("2j5kdv9ma") == 1
+        assert count_vowels("Öhm, Ich bin am testen.") == 5
+        print("count_vowels Test bestanden (Ausser ÄÖÜ)")
 
-    if find_max([1000,2,300,4,5,-600000]) == 1000:
+    def test_find_max():
+        assert find_max([1000,200,4,5,6]) == 1000
+        assert find_max([-5,-6,-1]) == -1
+        assert find_max([0,0,0,0]) == 0
+        assert find_max([-5,3,0]) == 3
         print("find_max Test bestanden")
-    
-    if bubble_sort([0,7,9,-4,5]) == [-4,0,5,7,9]:
+
+
+    def test_bubble_sort():
+        assert bubble_sort([0,7,9,-4,5]) == [-4,0,5,7,9]
+        assert bubble_sort([0,0,0,1,0]) == [0,0,0,0,1]
         print("bubble_sort Test bestanden")
 
-    if binary_search([-1,0,1,2,3,4,5,6,7,8,9,10],8) == 9:
-        print("binary_search Test bestanden")
+    def test_binary_search():
+        assert binary_search([-1,0,1,2,3,4,5,6,7,8,9,10],8) == 9
+        assert binary_search([0,1],1) == 1 
+        print("binary_search Test bestanden (Ausser wenn Element nicht in der Liste ist)")
 
-    def test_add():
-    assert add(1,3) == 4
-    assert add(-2,-3) == -5
-    assert add(5,-5) == 0
-    assert add(1000000, 1) == 1000001
-    print("add Test bestanden")
 
+    
+test_add()
+test_subtract()
+test_multiply()
+test_divide()
+test_is_even()
+test_max_of_two()
+test_min_of_two()
+test_factorial()
+test_is_palindrome()
+test_count_vowels()
+test_find_max()
+test_bubble_sort()
+test_binary_search()
